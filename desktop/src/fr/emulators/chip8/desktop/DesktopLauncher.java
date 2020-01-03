74e5b7b4e1a27130787d7cb7e98bce46f2b034c9
LeRoiDesKiwis <ferryantony1@gmail.com>
2020-01-03 18:17:16 +0100

    init commit

---
+++ desktop/src/fr/emulators/chip8/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package fr.emulators.chip8.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import fr.emulators.chip8.Main;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Main(), config);
+	}
+}
